db7134120ae3053ef313fa85f0af16b949d9e0ca
Raquel Tsuji <raqueltsuji@gmail.com>
2023-04-25 23:56:09 -0300

    Adicionando o metodo post

--- src/main/java/onboard/raquel/Semana03/domain/Anime.java
+++ src/main/java/onboard/raquel/Semana03/domain/Anime.java
@@ -1,5 +1,6 @@
 package onboard.raquel.Semana03.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,6 @@ import lombok.Data;
 public class Anime {
     private Long id;
     private String name;
+    //@JsonProperty("name")
+    //private String nameCharacter;
 }
